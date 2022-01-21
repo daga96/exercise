935ef438eda4270a001d16fb61336c0222c0cd8a
Dagmara <44023957+daga96@users.noreply.github.com>
2022-01-21 19:47:19 +0900

    React Hooks Exercises part 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useState, useReducer, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+export default function App() {
+  //useEffect
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    console.log("componentDidMount");
+
+    axios.get("https://jsonplaceholder.typicode.com/comments").then((res) => {
+      setData(res.data[0]);
+      console.log(res.data[0]);
+    });
+  }, []);
+
+  //useReduce
+
+  const reducerFunction = (state, action) => {
+    switch (action.type) {
+      case "increment":
+        return { count: state.count + 1 };
+      case "decrement":
+        return { count: state.count - 1 };
+      case "reset":
+        return { count: 0 };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducerFunction, { count: 0 });
+  const [counter, setCounter] = useState(0);
+  //function for useState
+  const raiseCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  const resetCounter = () => {
+    setCounter(0);
+  };
+
+  const lowerCounter = () => {
+    setCounter(counter - 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="exercise">
+        <h1> Counter Using Hooks: useState</h1>
+        <p>{counter}</p>
+        <button onClick={raiseCounter}>+</button>
+        <button onClick={lowerCounter}>-</button>
+        <button onClick={resetCounter}>Reset</button>
+      </div>
+      <div className="exercise">
+        <h1> Counter Using Hooks: useReducer</h1>
+        <p>{state.count}</p>
+        <button
+          onClick={() => {
+            dispatch({ type: "increment" });
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          +
+        </button>
+        <button
+          onClick={() => {
+            dispatch({ type: "decrement" });
+          }}
+        >
+          -
+        </button>
+
+        <button
+          onClick={() => {
+            dispatch({ type: "reset" });
+          }}
+        >
+          Reset
+        </button>
+      </div>
+
+      <div className="exercise">
+        <h1> API Call </h1>
+        <h3>email: {data.email}</h3>
+        <h4>Titile: {data.name}</h4>
+        <p> {data.body} </p>
+      </div>
     </div>
   );
 }
-
-export default App;
